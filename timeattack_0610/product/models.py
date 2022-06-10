e83e4914db73076a0203e4a2f9a4d0063a04317d
khw7876 <101394490+khw7876@users.noreply.github.com>
2022-06-10 17:17:20 +0900

    Add files via upload

---
+++ timeattack_0610/product/models.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from operator import mod
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+class product(models.Model):
+    class Meta:
+        db_table = 'product'
+    product_title = models.CharField(max_length=128,unique=True)
+    product_category = models.CharField(max_length=128,unique=True)
+    product_image = models.URLField()
+    product_intro = models.TextField()
+    product_price = models.IntegerField()
+    product_stock = models.IntegerField()
+
+class Category(models.Model):
+    class Meta:
+        db_table = "category"
+    category_name = models.ForeignKey(product, on_delete=models.CASCADE)
+
+class OrderStatus(models.Model):
+    class Meta:
+        db_table = 'order_Status'
+
+    order_complate = models.BooleanField()
+    order_complate = models.BooleanField()
+    order_complate = models.BooleanField()
+    order_complate = models.BooleanField()
+
+class ProductOrder(models.Model):
+    class Meta:
+        db_table = "produect_order"
+    product_count = models.IntegerField()
+
+class UserOrder(models.Model):
+    class Meta:
+        주문시간, 전체 상품 가격, 할인율, 최종가격, 유효여부(boolean) 
+        db_table = "User_Order"
+    user_address = models.TextField()
+    order_time = models.DateField()
+    all_price = models.IntegerField()
+    discount_per = models.IntegerField()
+    total_price = models.IntegerField()
+    order_valid = models.BooleanField()
